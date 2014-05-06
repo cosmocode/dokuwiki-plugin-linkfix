b1b955afd2e619edc01ca23c1b7b9d38f53887f6
Andreas Gohr <gohr@cosmocode.de>
2014-05-06 17:02:11 +0200

    first working version

--- admin.php
+++ admin.php
@@ -12,10 +12,10 @@ if(!defined('DOKU_INC')) die();
 class admin_plugin_linkfix extends DokuWiki_Admin_Plugin {
 
     private $searchin = '';
-    private $filter = '';
     private $changefrom = '';
     private $changeto = '';
     private $dryrun = false;
+    private $type = 'links';
 
     public function getMenuSort() {
         return 3000;
@@ -23,62 +23,75 @@ class admin_plugin_linkfix extends DokuWiki_Admin_Plugin {
 
     public function handle() {
         if(isset($_REQUEST['searchin'])) $this->searchin = cleanID($_REQUEST['searchin']);
-        if(isset($_REQUEST['filter'])) $this->filter = cleanID($_REQUEST['filter']);
-        if(isset($_REQUEST['changefrom'])) $this->changefrom = cleanID($_REQUEST['changefrom']);
-        if(isset($_REQUEST['changeto'])) $this->changeto = cleanID($_REQUEST['changeto']);
+        if(isset($_REQUEST['changefrom'])) $this->changefrom = $_REQUEST['changefrom'];
+        if(isset($_REQUEST['changeto'])) $this->changeto = $_REQUEST['changeto'];
+        if(isset($_REQUEST['type'])) $this->type = $_REQUEST['type'];
         if(isset($_REQUEST['dryrun'])) $this->dryrun = (bool) $_REQUEST['dryrun'];
     }
 
     public function html() {
+        global $ID;
 
         if(!empty($_REQUEST['go']) && checkSecurityToken()) {
-            echo "<h3>Processing, do not interrupt...</h3>";
+            echo '<h3>' . $this->getLang('processing') . '</h3>';
             tpl_flush();
 
             ignore_user_abort(true);
             set_time_limit(0);
-            $this->execute();
+            if($this->execute()) {
+                echo "<h3>Processing done.</h3>";
 
-            echo "<h3>Processing done.</h3>";
-
-            if($this->dryrun) {
-                echo '<a href="' . wl(
-                    '', array(
-                             'do' => 'admin',
-                             'page' => 'linkfix',
-                             'searchin' => $this->searchin,
-                             'filter' => $this->filter,
-                             'changefrom' => $this->changefrom,
-                             'changeto' => $this->changeto,
-                             'dryrun' => 0,
-                             'go' => 'go',
-                             'sectok' => getSecurityToken()
-                        )
-                ) . '">rerun the process for real (NO simulation!)</a>';
+                if($this->dryrun) {
+                    echo '<a href="' . wl(
+                            '', array(
+                                     'do'         => 'admin',
+                                     'page'       => 'linkfix',
+                                     'searchin'   => $this->searchin,
+                                     'changefrom' => $this->changefrom,
+                                     'changeto'   => $this->changeto,
+                                     'dryrun'     => 0,
+                                     'go'         => 'go',
+                                     'sectok'     => getSecurityToken()
+                                )
+                        ) . '">rerun the process for real (NO simulation!)</a>';
+                }
+                tpl_flush();
             }
-            tpl_flush();
         } else {
 
-            echo '<p>Don\'t use this unless you have any idea of what this is doing (eg. are a CosmoCode employee)</p>';
+            echo $this->locale_xhtml('intro');
 
-            $form = new Doku_Form(array());
+            $form = new Doku_Form(array('action' => wl()));
             $form->addHidden('go', 'go');
-            $form->startFieldset('Configure');
-            $form->addElement(form_makeTextField('searchin', '', 'Search in', '', 'block'));
-            $form->addElement(form_makeTextField('filter', '', 'Look for links to this namespace', '', 'block'));
-            $form->addElement(form_makeTextField('changefrom', '', 'Change from this prefix namespace', '', 'block'));
-            $form->addElement(form_makeTextField('changeto', '', 'Change to this prefix namespace', '', 'block'));
-            $form->addElement(form_makeCheckboxField('dryrun', '1', 'Simulate only', '', 'block', array('checked' => 'checked')));
-            $form->addElement(form_makeButton('submit', ''));
+            $form->addHidden('page', 'linkfix');
+            $form->addHidden('id', $ID);
+            $form->startFieldset($this->getLang('menu'));
+            $form->addElement(form_makeTextField('searchin', '', $this->getLang('searchin'), '', 'block'));
+            $form->addElement(form_makeTextField('changefrom', '', $this->getLang('changefrom'), '', 'block'));
+            $form->addElement(form_makeTextField('changeto', '', $this->getLang('changeto'), '', 'block'));
+            $form->addElement(form_makeRadioField('type', 'links', $this->getLang('links'), '', 'block', array('checked' => 'checked')));
+            $form->addElement(form_makeRadioField('type', 'media', $this->getLang('media'), '', 'block'));
+            $form->addElement(form_makeCheckboxField('dryrun', '1', $this->getLang('dryrun'), '', 'block', array('checked' => 'checked')));
+            $form->addElement(form_makeButton('submit', 'admin', $this->getLang('submit')));
             $form->printForm();
         }
     }
 
     /**
      * Search for all pages in the set namespace and update them
+     *
+     * @returns bool false if no correct namespace was given
      */
     private function execute() {
         global $conf;
+
+        $searchin = str_replace(':', '/', $this->searchin);
+        if(!is_dir($conf['datadir'] . '/' . $searchin)) {
+            msg(sprintf($this->getLang('badnamespace'), hsc($this->searchin)), -1);
+            return false;
+        }
+
+        // FIXME use index. When local link (not HTTP prefix) use metadata index, otherwise standard fulltext
 
         $data = array();
         search(
@@ -86,18 +99,20 @@ class admin_plugin_linkfix extends DokuWiki_Admin_Plugin {
             $conf['datadir'],
             'search_allpages',
             array(
-                 'depth' => 0,
+                 'depth'   => 0,
                  'skipacl' => true,
-                 'hash' => false,
+                 'hash'    => false,
             ),
-            str_replace(':', '/', $this->searchin)
+            $searchin
         );
 
         foreach($data as $file) {
-            print '<b>' . hsc($file['id']) . "</b><br />";
+            print $this->getLang('checking') . ' <b>' . hsc($file['id']) . "</b><br />";
             tpl_flush();
             $this->updatepage($file['id']);
         }
+
+        return true;
     }
 
     /**
@@ -108,16 +123,18 @@ class admin_plugin_linkfix extends DokuWiki_Admin_Plugin {
     private function updatepage($currentpage) {
         $currentns = getNS($currentpage);
 
-        $text = rawWiki($currentpage);
-        $crc = md5($text);
+        $text         = rawWiki($currentpage);
+        $crc          = md5($text);
         $instructions = p_get_instructions($text);
         $instructions = array_reverse($instructions);
 
         foreach($instructions as $instruction) {
-            if($instruction[0] == 'internallink' || $instruction[0] == 'internalmedia') {
-                $type = substr($instruction[0], 8); //strip "internal"
+            if(
+                ($this->type == 'links' && ($instruction[0] == 'internallink' || $instruction[0] == 'externallink')) ||
+                ($this->type == 'media' && ($instruction[0] == 'internalmedia' || $instruction[0] == 'externalmedia'))
+            ) {
                 $link = $instruction[1][0];
-                $pos = $instruction[2] - 1;
+                $pos  = $instruction[2] - 1;
 
                 while(
                     $text{$pos} == '[' ||
@@ -128,17 +145,12 @@ class admin_plugin_linkfix extends DokuWiki_Admin_Plugin {
                     $pos++;
                 }
 
-                $full = $link;
+                $full   = $link;
                 $exists = false;
-                if($type == 'page') {
+                if($this->type == 'links') {
                     resolve_pageid($currentns, $full, $exists);
                 } else {
                     resolve_mediaid($currentns, $full, $exists);
-                }
-
-                if($this->filter && strpos($full, "$this->filter:") !== 0) {
-                    // current link does not match filter, ignore
-                    continue;
                 }
 
                 // create the new link
@@ -146,7 +158,7 @@ class admin_plugin_linkfix extends DokuWiki_Admin_Plugin {
 
                 // replace the link
                 if(($link != $newlink) && ($full != cleanID($newlink))) {
-                    echo '&nbsp;&nbsp;&nbsp;' . hsc($link) . ' -> ' . hsc($newlink) . '<br />';
+                    echo '&nbsp;&nbsp;&nbsp;' . hsc($link) . ' → ' . hsc($newlink) . '<br />';
 
                     $text = substr($text, 0, $pos) .
                         $newlink . // new link
@@ -157,13 +169,13 @@ class admin_plugin_linkfix extends DokuWiki_Admin_Plugin {
             // everything else is ignored
         }
         if($crc == md5($text)) {
-            echo '✗ page not changed<br />';
+            echo '✗ '.$this->getLang('fail').'<br />';
         } else {
             if($this->dryrun) {
-                echo '✓ simulating, page changes not saved<br />';
+                echo '✓ '.$this->getLang('successdry').'<br />';
             } else {
-                saveWikiText($currentpage, $text, 'links adjusted', true);
-                echo '✓ new page version saved <br />';
+                saveWikiText($currentpage, $text, $this->getLang('summary'), true);
+                echo '✓ '.$this->getLang('success').'<br />';
             }
         }
         tpl_flush();
@@ -172,25 +184,22 @@ class admin_plugin_linkfix extends DokuWiki_Admin_Plugin {
     /**
      * Rewrite the given link according to the given settings
      *
-     * @param string $link the current link as found in the page source
-     * @param string $full the full, resolved version of the above link
+     * @param string $link      the current link as found in the page source
+     * @param string $full      the full, resolved version of the above link
      * @param string $currentNS the namespace of the page the link was found in
      * @return string the corrected link
      */
     private function changeLink($link, $full, $currentNS) {
         // make sure the prefix matches
-        if($this->changefrom && strpos($full, "$this->changefrom:") !== 0) return $link;
+        if($this->changefrom && strpos($full, "$this->changefrom") !== 0) return $link;
 
         // strip prefix
         $new = substr($full, strlen($this->changefrom));
 
-        // strip left over colon
-        $new = ltrim($new, ':');
-
         // add prefix
-        if($this->changeto) $new = $this->changeto . ':' . $new;
+        $new = $this->changeto . $new;
 
-        // strip left over colon
+        // strip left over colons
         $new = ltrim($new, ':');
 
         // make absolute if needed
---
+++ lang/en/lang.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * English language file for linkfix plugin
+ *
+ * @author Andreas Gohr <gohr@cosmocode.de>
+ */
+
+// menu entry for admin plugins
+$lang['menu'] = 'Adjust links';
+
+$lang['searchin']     = 'Limit to this namespace:';
+$lang['changefrom']   = 'Change links beginning with:';
+$lang['changeto']     = 'Change links to begin with:';
+$lang['dryrun']       = 'Simulate changes for testing';
+$lang['submit']       = 'Start';
+$lang['processing']   = 'Processing, do not interrupt...';
+$lang['badnamespace'] = 'No such namespace %s. Aborting execution.';
+$lang['links']        = 'Adjust Links';
+$lang['media']        = 'Adjust Media';
+$lang['summary']      = 'links adjusted';
+$lang['success']      = 'new page version saved';
+$lang['successdry']   = 'simulating, page changes not saved';
+$lang['fail']         = 'page not changed';
+$lang['checking']     = 'checking page:';
+
+//Setup VIM: ex: et ts=4 :
