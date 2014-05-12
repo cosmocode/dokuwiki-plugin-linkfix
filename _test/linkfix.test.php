32fa8095a76d4d61d726059236b0c74db43db641
Andreas Gohr <gohr@cosmocode.de>
2014-05-12 11:30:37 +0200

    added some more tests

--- _test/linkfix.test.php
+++ _test/linkfix.test.php
@@ -17,23 +17,53 @@ class linkfix_plugin_linkfix_test extends DokuWikiTest {
     }
 
 
-    public function test_plugininfo() {
+    public function test_internal() {
         $plugin = new test_admin_plugin_linkfix();
 
         $plugin->changefrom = 'linkfix:';
         $plugin->changeto   = 'linkfox:';
-
-        ob_start();
         $plugin->execute();
-        ob_end_clean();
 
         $text = rawWiki('linkfix:linkfix');
-
-        $this->assertRegExp('/linkfox:linkfix|case2/', $text);
-
-
+        $this->assertRegExp('/linkfox:linkfix\|case 1/', $text);
     }
 
+    public function test_external() {
+        $plugin = new test_admin_plugin_linkfix();
+
+        $plugin->changefrom = 'http://www.google';
+        $plugin->changeto   = 'http://www.foogle';
+        $plugin->isextern   = true;
+        $plugin->execute();
+
+        $text = rawWiki('linkfix:linkfix');
+        $this->assertRegExp('/http:\/\/www\.foogle\.com\|case 2/', $text);
+    }
+
+    public function test_internalmedia() {
+        $plugin = new test_admin_plugin_linkfix();
+
+        $plugin->changefrom = 'something:logo.';
+        $plugin->changeto   = 'something:icon.';
+        $plugin->type = 'media';
+        $plugin->execute();
+
+        $text = rawWiki('linkfix:linkfix');
+        $this->assertRegExp('/\{\{ something:icon\.png \|case 3\}\}/', $text);
+    }
+
+    public function test_externalmedia() {
+        $plugin = new test_admin_plugin_linkfix();
+
+        $plugin->changefrom = 'http://www.google';
+        $plugin->changeto   = 'http://www.foogle';
+        $plugin->isextern   = true;
+        $plugin->type = 'media';
+        $plugin->execute();
+
+        $text = rawWiki('linkfix:linkfix');
+        $this->assertRegExp('/{{ http:\/\/www\.foogle\.com\/logo.png |case 4}}/', $text);
+    }
 }
 
 class test_admin_plugin_linkfix extends admin_plugin_linkfix {
@@ -42,6 +72,7 @@ class test_admin_plugin_linkfix extends admin_plugin_linkfix {
     public $changeto = '';
     public $dryrun = false;
     public $type = 'links';
+    public $isextern = false;
 
     public function __construct() {
 
